c1746e5605c1073e8c7ce541fadbfcb2c73aba23
omer <omerb@codevalue.net>
2019-08-12 08:30:55 +0300

    removed unneeded info

--- aks/init.tf
+++ aks/init.tf
@@ -30,23 +30,6 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
 
-/*provider "helm" {
-  debug           = true
-  version         = "~> 0.10"
-  namespace       = "kube-system"
-  service_account = "tiller"
-  install_tiller  = "true"
-  kubernetes {
-    host     = azurerm_kubernetes_cluster.aks.kube_config.0.host
-    username = azurerm_kubernetes_cluster.aks.kube_config.0.username
-    password = azurerm_kubernetes_cluster.aks.kube_config.0.password
-
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
-  }
-}*/
-
 terraform {
   required_version = ">= 0.12"
   backend "azurerm" {}
--- aks/output.tf
+++
@@ -1,7 +0,0 @@
-/*output "dns_zone_name_servers" {
-  value = azurerm_dns_zone.dns_zone.name_servers
-}
-
-output "kube_config_raw" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw 
-}*/
