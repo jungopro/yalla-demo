6fac489e287fcd9b31ad80a3a8afa90cc90bcd39
omer <omerb@codevalue.net>
2019-08-21 09:23:47 +0300

    formatted and removed unneeded var

--- aks/variables.tf
+++ aks/variables.tf
@@ -4,7 +4,7 @@ variable "client_secret" {
 
 variable "location" {
   description = "Azure Location"
-  default = "west europe"
+  default     = "west europe"
 }
 
 variable "kubeconfig_path" {
@@ -13,53 +13,48 @@ variable "kubeconfig_path" {
 
 variable "node_count" {
   description = "the number of worker nodes in the pool"
-  default = 3
+  default     = 3
 }
 
 variable "max_pods" {
   description = "The maximum number of pods that can run on each agent"
-  default = 30
+  default     = 30
 }
 
 variable "aks_rg_name" {
   description = "the name of the rg for the aks cluster"
-  default = "aks"
+  default     = "aks"
 }
 
 variable "aks_subnet_name" {
   description = "the name of the subnet for the aks nodes"
-  default = "aks-subnet"
+  default     = "aks-subnet"
 }
 
 variable "vnet_name" {
   description = "the name of the vnet"
-  default = "aks-vnet"
+  default     = "aks-vnet"
 }
 
 variable "vnet_address_space" {
   description = "list of address spaces for the vnet"
   type        = list(string)
-  default = ["10.0.0.0/8"]
+  default     = ["10.0.0.0/8"]
 }
 
 variable "aks_subnet_address" {
   description = "the network address for the aks subnet"
-  default = "10.200.0.0/22"
-}
-
-variable "vk_subnet_address" {
-  description = "the network address for the aks subnet"
-  default = "10.241.0.0/24"
+  default     = "10.200.0.0/22"
 }
 
 variable "cluster_name" {
   description = "the name of the aks cluster. also the dns prefix"
-  default = "playks"
+  default     = "playks"
 }
 
 variable "kubernetes_version" {
   description = "the kubernetes version to use"
-  default = "1.14.5"
+  default     = "1.14.5"
 }
 
 variable "vm_size" {
